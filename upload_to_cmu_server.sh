e10955ca8a53f644f9659eef152d605870de96c0
Sireesh Gururaja <sireesh.gururaja@gmail.com>
2025-12-12 16:48:57 -0500

    make sure site gets rebuilt

--- upload_to_cmu_server.sh
+++ upload_to_cmu_server.sh
@@ -1,1 +1,8 @@
+set -euxo pipefail
+
+docker compose up --detach
+
+echo "Waiting in case Docker container needs it..."
+sleep 2
+
 rsync -avP --exclude='*.mp4' _site/* sgururaj@linux.gp.cs.cmu.edu:/afs/cs.cmu.edu/user/sgururaj/www
